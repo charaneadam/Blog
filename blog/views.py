49c7c1a0a73d67fa87cc92b85a0fcde4439227fb
Adam Charane <adamcharane@gmail.com>
2019-11-11 15:57:41 +0100

    Sidebar sections (Latest, Archive, Categories, Tags) finished

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ import markdown
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-from .models import Post
+from .models import Post, Category, Tag
 
 
 def index(request):
@@ -24,3 +24,20 @@ def detail(request, pk):
     post.body = md.convert(post.body)
     post.toc = md.toc
     return render(request, "blog/detail.html", context={'post': post})
+
+
+def archive(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def tag(request, pk):
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=t).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
