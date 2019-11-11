0818729d0a30faa4724ffa9594216ce7ada1565b
Adam Charane <adamcharane@gmail.com>
2019-11-11 17:02:43 +0100

    Comment form works finished. You can add your comments on articles now :D

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
+from django.contrib import messages
 
-# Create your views here.
+from blog.models import Post
+from .forms import CommentForm
+
+@require_POST
+def comment(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+
+    form = CommentForm(request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+        messages.success(request ,'Comment added successfully')
+        return redirect(post)
+
+    context = {
+        'form': form,
+        'post': post,
+    }
+
+    messages.error(request, 'Comment was not published. Please resubmit after taking care of the errors.')
+
+    template = 'comments/preview.html'
+
+    return render(request, template_name=template, context=context)
