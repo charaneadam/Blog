1f16ed06c96e41ddc7b9a00af1cfd43969aa6a64
Adam Charane <adamcharane@gmail.com>
2019-11-11 16:19:27 +0100

    comment application and Comment model are created

---
+++ comments/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+class Comment(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    url = models.URLField(blank=True)
+    text = models.TextField()
+    created_time = models.DateTimeField(default=timezone.now)
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}: {self.text[:20]}'
