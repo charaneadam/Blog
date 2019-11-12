2ead60361485c9fe850252c361d6156a0dea3075
Adam Charane <adamcharane@gmail.com>
2019-11-12 17:53:05 +0100

    Turning archive views to one class based view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 import markdown
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpRequest, HttpResponse
 from django.views.generic import ListView
 from django.contrib.auth.models import User
@@ -18,37 +18,27 @@ class CategoryView(IndexView):
         cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
         return super(CategoryView, self).get_queryset().filter(category=cate)
 
+
 class AuthorView(IndexView):
     def get_queryset(self):
         auth = get_object_or_404(User, pk=self.kwargs.get('pk'))
         return super(AuthorView, self).get_queryset().filter(author=auth)
+
 
 class TagView(IndexView):
     def get_queryset(self):
         tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
         return super(TagView, self).get_queryset().filter(tags=tag)
 
-############################# TODO: Turn these views to class based views #############################
-def archive(request, year, month):
-    post_list = Post.objects.filter(created_time__year=year,
-                                    created_time__month=month
-                                    )
-    return __render(request, {'post_list': post_list})
 
 class ArchiveView(IndexView):
     def get_queryset(self):
-        cate = get_object_or_404(Category, year=self.kwargs.get('year'), month=self.kwargs.get('month'))
-        return super(ArchiveView, self).get_queryset().filter(category=cate)
-
-def archive_day(request, year, month, day=None):
-    post_list = Post.objects.filter(created_time__year=year,
-                                    created_time__month=month,
-                                    created_time__day=day
-                                    )
-    return __render(request, {'post_list': post_list})
-
-############################# TODO (END): Turn these views to class based ...  #############################
-
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        day = self.kwargs.get('day')
+        if day:
+            return Post.objects.filter(created_time__year=year, created_time__month=month, created_time__day=day)
+        return Post.objects.filter(created_time__year=year, created_time__month=month)
 
 
 def detail(request, pk):
@@ -63,8 +53,3 @@ def detail(request, pk):
     post.reads = post.reads + 1
     post.save()
     return render(request, "blog/detail.html", context={'post': post})
-
-
-def __render(req: HttpRequest, context: dict = dict(), template: str = 'blog/index.html', ) -> HttpResponse:
-    return render(req, template_name=template, context=context)
-
